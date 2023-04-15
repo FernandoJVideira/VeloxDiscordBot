98d9da15a6cd5b8f2e999f1bba058b0b74620167
Fernando Videira <tragedy_twisty.0t@icloud.com>
2023-04-15 12:10:25 +0100

    Removed commented old way of user authentication

--- EventHandler.py
+++ EventHandler.py
@@ -2,16 +2,16 @@ import nextcord
 from nextcord.ext import commands
 
 intents = nextcord.Intents().all()
-bot = commands.Bot(command_prefix = '.ds ', intents = intents)
-ROLE_ID = 1074785999336509565 #TODO: change this to file with role id
+bot = commands.Bot(command_prefix='.ds ', intents=intents)
+ROLE_ID = 1074785999336509565  # TODO: change this to database connection
 
 
 class eventHandler(commands.Cog):
-    
+
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()   
+    @commands.Cog.listener()
     async def on_ready(self):
         print("Ready!")
 
@@ -24,28 +24,6 @@ class eventHandler(commands.Cog):
             role = guild.get_role(ROLE_ID)
             await after.add_roles(role)
 
-    #@commands.Cog.listener() 
-    #async def on_raw_reaction_add(self, payload : nextcord.RawReactionActionEvent):
-    #    guild = self.bot.get_guild(payload.guild_id)
-    #    emoji = payload.emoji.name
-    #    message_id = payload.message_id
-    #    
-    #    #TODO: change message_id to the message id of the message you want to react to
-    #    if emoji == "✅" and message_id == 1074787938354864311:
-    #        role = nextcord.utils.get(guild.roles, name = "👤| Membro")
-    #        await payload.member.add_roles(role)
-
-    #@commands.Cog.listener() 
-    #async def on_raw_reaction_remove(self, payload : nextcord.RawReactionActionEvent):
-    #    guild = self.bot.get_guild(payload.guild_id)
-    #    emoji = payload.emoji.name
-    #    message_id = payload.message_id
-    #    member = await guild.fetch_member(payload.user_id)
-
-    #    #TODO: change message_id to the message id of the message you want to react to
-    #    if emoji == "✅" and message_id == 1074787938354864311:
-    #        role = nextcord.utils.get(guild.roles, name = "👤| Membro")
-    #        await member.remove_roles(role)
 
 def setup(bot):
     bot.add_cog(eventHandler(bot))
