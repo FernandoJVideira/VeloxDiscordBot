d4e6d3be6390c90c8340c7892cdd8d59a6352d1f
Fernando Videira <tragedy_twisty.0t@icloud.com>
2023-11-02 02:24:50 +0000

    Removed on_member_join event to EventHandler

--- bot.py
+++ bot.py
@@ -19,29 +19,8 @@ class MyBot(commands.Bot):
         await bot.tree.sync(guild=discord.Object(id=1169255234598600804))
 
     async def on_ready(self):
-        print("Bot is ready")
+        print("Ready for action!")
 
 bot = MyBot()
-
-@bot.event
-async def on_member_join(member):
-    guild = member.guild
-    guildchannel = discord.utils.get(guild.channels, name="welcome")
-    dmchannel = await member.create_dm()
-    await dmchannel.send(f"Welcome to **{guild.name}**! Have fun!")
-
-    if guildchannel is not None:
-        # Welcome Embed
-        MyEmbed = discord.Embed(
-            title="👋 Bem Vindo(a)!", description=f"Olá {member.mention}, espero que te divirtas no server {guild.name}", color=discord.Colour.orange())
-        MyEmbed.set_author(
-            name=f"{member.name} #{member.discriminator}", icon_url=member.display_avatar.url)
-        MyEmbed.set_thumbnail(url=member.display_avatar.url)
-        MyEmbed.set_image(
-            url="https://media.giphy.com/media/61XS37iBats8J3QLwF/giphy.gif")
-        MyEmbed.set_footer(text=f"ID: {member.id}")
-
-        await guildchannel.send(member.mention, embed=MyEmbed)
-
 
 bot.run(os.getenv("TOKEN"))
--- cogs/EventHandler.py
+++ cogs/EventHandler.py
@@ -18,6 +18,26 @@ class eventHandler(commands.Cog):
             role = guild.get_role(ROLE_ID)
             await after.add_roles(role)
 
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        guild = member.guild
+        guildchannel = discord.utils.get(guild.channels, name="welcome")
+        dmchannel = await member.create_dm()
+        await dmchannel.send(f"Welcome to **{guild.name}**! Have fun!")
+
+        if guildchannel is not None:
+            # Welcome Embed
+            MyEmbed = discord.Embed(
+                title="👋 Welcomeee!", description=f"{member.mention}! Welcome to the shitshowww!", color=discord.Colour.orange())
+            MyEmbed.set_author(
+                name=f"{member.name} #{member.discriminator}", icon_url=member.display_avatar.url)
+            MyEmbed.set_thumbnail(url=member.display_avatar.url)
+            MyEmbed.set_image(
+                url="https://media.giphy.com/media/61XS37iBats8J3QLwF/giphy.gif")
+            MyEmbed.set_footer(text=f"ID: {member.id}")
+
+            await guildchannel.send(member.mention, embed=MyEmbed)
+
 
 async def setup(bot):
-    await bot.add_cog(eventHandler(bot))
+    await bot.add_cog(eventHandler(bot), guilds=[1169255234598600804])
